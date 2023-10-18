c234ce45b6da8eb8a8e5c93c1534668c85d98d22
HyunJae Kim <kimhun456@gmail.com>
2023-10-18 15:31:30 +0900

    Add delete method

--- src/main/kotlin/com/example/spring_mvc/TodoController.kt
+++ src/main/kotlin/com/example/spring_mvc/TodoController.kt
@@ -1,10 +1,13 @@
 package com.example.spring_mvc
 
 import com.example.spring_mvc.data.service.TodoService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/todo")
@@ -15,5 +18,23 @@ class TodoController(private val todoService: TodoService) {
         val todos = todoService.getAllTodos()
         model.addAttribute("todoList", todos)
         return "list"
+    }
+
+    @PostMapping("/add")
+    fun addList(@RequestParam("todo") todo: String): String {
+        logger.info("todo is $todo")
+        todoService.addTodo(todo)
+        return "redirect:/todo/list"
+    }
+
+    @GetMapping("/delete")
+    fun deleteTodo(@RequestParam("id") id: Long): String {
+        logger.info("deleteTodo id: $id")
+        todoService.deleteTodo(id)
+        return "redirect:/todo/list"
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(TodoController::class.java)
     }
 }
--- src/main/webapp/WEB-INF/views/list.jsp
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -7,11 +7,17 @@
 <body>
 <h1>TODO list</h1>
 
+<form action="add" method="post">
+    todo : <input type="text" name="todo" placeholder="input todo"/>
+    <input type="submit" value="add todo">
+</form>
+
 <c:forEach items="${todoList}" var="todo">
     <p>
         <c:if test="${todo.done}">Complete</c:if>
         <c:if test="${!todo.done}">Progressing</c:if>
-            ${todo.todo}
+            ${todo.todo} &nbsp;&nbsp;
+        <a href="delete?id=${todo.id}">DELETE</a>
     </p>
 </c:forEach>
 </body>
